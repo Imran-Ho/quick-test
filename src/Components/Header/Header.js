a71646e04602c1d622508baf898774a833aa0b53
Imran-Ho <imranhossain36@gmail.com>
2022-11-08 12:04:35 +0600

    header and nav section created

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import image from '../../Sources/dispensary.jpg'
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='grid lg:grid-cols-2 bg-slate-300 mt-5 mx-5'>
+            <img className='m-3' src={image} alt="" />
+            <div className='text-center p-10'>
+                <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam adipisci incidunt quidem voluptate modi quod, autem in numquam deleniti mollitia minima temporibus similique vel nihil illo maxime quisquam dolores possimus?</h2>
+            </div>
         </div>
     );
 };
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from '../Header/Header';
 import NavBar from './NavBar/NavBar';
 
 
@@ -6,6 +7,7 @@ const Home = () => {
     return (
         <div>
             <NavBar></NavBar>
+            <Header></Header>
         </div>
     );
 };
