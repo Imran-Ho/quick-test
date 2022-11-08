68c08b8ea6f973cd325cdbcbaba0603d259a2c2c
Imran-Ho <imranhossain36@gmail.com>
2022-11-08 18:35:28 +0600

    brought data from DB

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from './layer/Main';
+import Service from './Components/Service/Service';
 
 function App() {
   const router = createBrowserRouter([
     {
-      path:'/', element: <Main></Main>
+      path:'/', element: <Main></Main>,
+      children:[
+        {
+          path:'/tests', element: <Service></Service>
+        }
+      ]
     }
   ])
   return (
--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import TestItems from '../TestItems/TestItems';
 
 const Service = () => {
+    const [testItems, setTestItems] = useState([])
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/tests')
+        .then(res => res.json())
+        .then(data => setTestItems(data))
+    }, [])
     return (
         <div>
-            <h3>service</h3>
+            <h3>service: {testItems.length}</h3>
+            {
+                testItems.map(singleItem => <TestItems
+                key={singleItem._id}
+                singleItem={singleItem}
+                ></TestItems>)
+            }
         </div>
     );
 };
